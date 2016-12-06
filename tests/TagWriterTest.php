3a5886910e369c6e76b52e6ac81233b73438ab0f
Tsvetan Nenov <tsvetan.nenov@alea.com>
2016-12-06 13:53:47 +0100

    Some tests aded

---
+++ tests/TagWriterTest.php
@@ -0,0 +1,37 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Enables\TagWriter;
+
+class TagWriterTest extends TestCase
+{
+    /** @var  \Enables\TagWriter $tagWriter */
+    private $tagWriter;
+
+    /**
+     * Simplified setup:
+     *  OpenTag == char
+     *  ClosingTag = ~char
+     */
+    protected function setUp() {
+        $this->tagWriter = new TagWriter();
+
+        $this->tagWriter->setOpenTag("%s");
+        $this->tagWriter->setClosingTag("~%s");
+        $this->tagWriter->setNewLineCharacter("");
+        $this->tagWriter->setOffsetCharacter("");
+    }
+
+    public function testEmpty() {
+        $this->assertEmpty($this->tagWriter->process(""));
+    }
+
+    public function testItOutputsClosingTags() {
+        $this->assertEquals("abcdef~f~e~d~c~b~a", $this->tagWriter->process("abcdef"));
+    }
+
+    public function testItOutputsLevels() {
+        $this->tagWriter->setOffsetCharacter(" ");
+        $this->assertEquals("a b  c  ~c ~b~a", $this->tagWriter->process("abc"));
+    }
+
+}
